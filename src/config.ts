381f19c2405bbc7a36ddefca2ad96581f31d52cd
Aryan Bachchu <aryanbachchu150@gmail.com>
2025-03-30 09:26:11 +0530

    add config_sample.ts & untrack config.ts

--- src/config.ts
+++
@@ -1,4 +0,0 @@
-export const PORT = 3000;
-export const DB_URL = "mongodb+srv://aryanbachchu150:Kalidas1234@cluster0.sph77.mongodb.net";
-export const DB_NAME = "payTm";
-export const JWT_SECRET = "RANDOM@123";
---
+++ src/config_sample.ts
@@ -0,0 +1,4 @@
+export const PORT = 3000;
+export const DB_URL = "";
+export const DB_NAME = "";
+export const JWT_SECRET = "";
