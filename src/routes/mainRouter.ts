57238e967dd421984db302357848803267b35b5c
Aryan Bachchu <aryanbachchu150@gmail.com>
2025-10-19 21:19:43 +0530

    Add: health check end point for testing

--- src/routes/mainRouter.ts
+++ src/routes/mainRouter.ts
@@ -5,3 +5,9 @@ export const mainRouter = Router();
 
 mainRouter.use("/user", userRouter);
 mainRouter.use("/account", accountRouter);
+
+mainRouter.get("/health-check", (req, res) => {
+  res.status(200).json({
+    msg: "hii there"
+  })
+});
