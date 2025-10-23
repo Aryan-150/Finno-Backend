d805ee24177ccd2515d59a4a3cb1c00315464751
Aryan Bachchu <aryanbachchu150@gmail.com>
2025-10-23 13:33:38 +0530

    added frontend url as cors origin

--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,12 @@ type User = {
 
 declare module 'express-session' {
     interface SessionData {
-      token: string;    // extend the sessionData with token
-      userId ?: mongoose.Types.ObjectId;
-      user: User
+        token: string;    // extend the sessionData with token
+        userId?: mongoose.Types.ObjectId;
+        user: User
     }
 }
-  
+
 declare global {
     namespace Express {
         interface Request {
@@ -31,8 +31,16 @@ declare global {
 
 const app = express();
 app.use(express.json());
+const whitelist = ['https://finno.aryanbachchu.tech', 'http://localhost:5173']
+
 app.use(cors({
-    origin: "http://localhost:5173", // Replace with your frontend's URL
+    origin: function (origin, callback) {
+        if (origin && whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
     credentials: true, // Allow credentials (cookies)
 }));
 app.use(session({
@@ -46,10 +54,10 @@ app.use("/api/v1", mainRouter);
 async function main() {
     await mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`);
     console.log('db connected');
-    
+
     app.listen(process.env.PORT, () => {
         console.log('server is listening at port: ' + process.env.PORT);
     })
-} 
+}
 
 main();
